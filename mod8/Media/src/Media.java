a5c51e05d65d34c6a999b52f23a2fbcbbadf4cf2
matheus-l-pereira <matheuslp.mjs@gmail.com>
2025-08-02 09:13:25 -0300

    Exercicio do modulo 8

---
+++ mod8/Media/src/Media.java
@@ -0,0 +1,19 @@
+public class Media {
+
+    public static void main(String[] args) {
+        int nota1 = 10;
+        int nota2 = 8;
+        int nota3 = 6;
+        int nota4 = 1;
+
+        double media = (nota1 + nota2 + nota3 + nota4) / 4;
+
+        System.out.println("******* Notas de fulano ******* ");
+        System.out.println("Nota da prova 1: " + nota1);
+        System.out.println("Nota da prova 2: " + nota2);
+        System.out.println("Nota da prova 3: " + nota3);
+        System.out.println("Nota da prova 4: " + nota4);
+        System.out.println("A média das notas é igual a " + media);
+
+    }
+}
