046c95e4c229ea0d1c92b7a15938bf787c3822ff
matheus-l-pereira <matheuslp.mjs@gmail.com>
2025-08-04 21:05:54 -0300

    Exercicio modulo 9

---
+++ mod9/Wrapper/src/br/com/MatheusPereira/ConverterWrapper.java
@@ -0,0 +1,13 @@
+package br.com.MatheusPereira;
+
+public class ConverterWrapper {
+
+    public static void main (String [] args){
+
+        int num1 = 50;
+        System.out.println(num1);
+
+        Integer num2 = num1;
+        System.out.println(num2);
+    }
+}
