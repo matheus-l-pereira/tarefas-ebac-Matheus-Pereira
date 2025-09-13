c8f9bc3b478797856c26b0cf85e8bcff419aafa0
matheus-l-pereira <matheuslp.mjs@gmail.com>
2025-09-13 14:31:36 -0300

    Exercicio do modulo 14

---
+++ mod14/CadastroCliente/src/br/com/matheus/pereira/dao/IClienteDAO.java
@@ -0,0 +1,18 @@
+package br.com.matheus.pereira.dao;
+
+import br.com.matheus.pereira.domain.Cliente;
+
+import java.util.Collection;
+
+public interface IClienteDAO {
+
+    public Boolean cadastrar (Cliente cliente);
+
+    public void excluir(Long cpf);
+
+    public void alterar(Cliente cliente);
+
+    public Cliente consultar(Long cpf);
+
+    public Collection<Cliente> buscarTodos();
+}
