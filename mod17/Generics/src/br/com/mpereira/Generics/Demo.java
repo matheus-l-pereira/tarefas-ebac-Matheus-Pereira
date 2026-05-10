a63663b3bb142b1ea4d01c03e7f58e76d400000c
matheus-l-pereira <matheuslp.mjs@gmail.com>
2026-05-10 19:32:56 -0300

    Exercicio mod17 corrigido

--- mod17/Generics/src/br/com/mpereira/Generics/Demo.java
+++ mod17/Generics/src/br/com/mpereira/Generics/Demo.java
@@ -1,23 +1,22 @@
 package br.com.mpereira.Generics;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Demo {
     public static void main(String[] args) {
-        // Garagem só para carros da marca Honda
-        Garagem<Honda> garagemHonda = new Garagem<>();
-        garagemHonda.adicionarCarro(new Honda("Civic"));
-        garagemHonda.adicionarCarro(new Honda("Fit"));
-        garagemHonda.adicionarCarro(new Honda("City"));
+        // Garagem para todos os carros
+        List<Carro> listaCarros = new ArrayList<>();
+        listaCarros.add(new Honda("Civic"));
+        listaCarros.add(new Toyota("Hilux"));
+        listaCarros.add(new Honda("Fit"));
+        listaCarros.add(new Toyota("Corola"));
+        listaCarros.add(new Honda("City"));
+        listaCarros.add(new Toyota("Etios"));
 
-        // Garagem só para carros da marca Toyota
-        Garagem<Toyota> garagemToyota = new Garagem<>();
-        garagemToyota.adicionarCarro(new Toyota("Corola"));
-        garagemToyota.adicionarCarro(new Toyota("Hilux"));
-        garagemToyota.adicionarCarro(new Toyota("Etios"));
-
-        System.out.println("---- Garagem Honda -----");
-        garagemHonda.exibirCarros();
-
-        System.out.println("---- Garagem Toyota -----");
-        garagemToyota.exibirCarros();
+        System.out.println("---- Garagem Carros -----");
+        for(Carro carro: listaCarros){
+            System.out.println(carro);
+        }
     }
 }
